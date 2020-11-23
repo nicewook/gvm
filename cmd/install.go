8cc6803d0320f357b608f4e711b51bf8143a53d0
Jeong Hyunseok <hsjeong@irisid.com>
2020-11-24 08:00:45 +0900

    add some explanation for the commands

--- cmd/install.go
+++ cmd/install.go
@@ -29,13 +29,9 @@ import (
 // installCmd represents the install command
 var installCmd = &cobra.Command{
 	Use:   "install",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Download and install your desired go version",
+	Long: `Download and install any go version you want.
+	Just specify the version number without "go" prefix.`,
 	Run: install,
 }
 
--- cmd/root.go
+++ cmd/root.go
@@ -50,13 +50,10 @@ var (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gvm",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: `"gvm" is go language version manager for Windows`,
+	Long: `"gvm" is go language version manager for Windows.
+	You can install/uninstall any version you want to use
+	and choose the version what you want for the time`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
