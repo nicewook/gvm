4db267f785d3487ba6dc50472562a7a1bf39797a
Jeong Hyunseok <hsjeong@irisid.com>
2020-12-03 21:15:16 +0900

    Clean up root.go

--- cmd/root.go
+++ cmd/root.go
@@ -16,15 +16,12 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/spf13/cobra"
 
-	homeDir "github.com/mitchellh/go-homedir"
 	goverbose "github.com/paulvollmer/go-verbose"
-	"github.com/spf13/viper"
 )
 
 const (
@@ -51,9 +48,9 @@ var (
 var rootCmd = &cobra.Command{
 	Use:   "gvm",
 	Short: `"gvm" is go language version manager for Windows`,
-	Long: `"gvm" is go language version manager for Windows.
-You can install/uninstall any version you want to use
-and choose the version what you want for the time`,
+	Long: `"gvm" is go language version manager for Windows. 
+You can install/uninstall any version you want to use 
+and choose the version what you want to use.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
@@ -63,8 +60,7 @@ and choose the version what you want for the time`,
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		log.Fatal(err)
 	}
 }
 
@@ -75,12 +71,12 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gvm.yaml)")
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gvm.yaml)")
 	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose output")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
 	// log settings
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
@@ -88,38 +84,39 @@ func init() {
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homeDir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+	/*
+		if cfgFile != "" {
+			// Use config file from the flag.
+			viper.SetConfigFile(cfgFile)
+		} else {
+			// Find home directory.
+			home, err := homeDir.Dir()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			// Search config in home directory with name ".gvm" (without extension).
+			viper.AddConfigPath(home)
+			viper.SetConfigName(".gvm")
 		}
 
-		// Search config in home directory with name ".gvm" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".gvm")
+		viper.AutomaticEnv() // read in environment variables that match
 
-		// Set verbose https://play.golang.org/p/RoRcgJV0pDV
-		fmtV = goverbose.New(os.Stdout, verbose)
+		// If a config file is found, read it in.
+		if err := viper.ReadInConfig(); err == nil {
+			fmt.Println("Using config file:", viper.ConfigFileUsed())
+		}
+	*/
+	// Set verbose https://play.golang.org/p/RoRcgJV0pDV
+	fmtV = goverbose.New(os.Stdout, verbose)
 
-		// JHS custom config
-		goRoot = os.Getenv("GOROOT")
-		goPath = os.Getenv("GOPATH")
-		fmtV.Println("GOROOT:", goRoot)
-		fmtV.Println("GOPATH:", goPath)
+	// JHS custom config
+	goRoot = os.Getenv("GOROOT")
+	goPath = os.Getenv("GOPATH")
+	fmtV.Println("GOROOT:", goRoot)
+	fmtV.Println("GOPATH:", goPath)
 
-		// init go version list installed in local
-		goVerList = getLocalList()
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
+	// init go version list installed in local
+	goVerList = getLocalList()
 }
